58c27814a5dae2486aeeb07345d45a19543e08d0
Pollob <paulpollob71@gmail.com>
2022-05-13 10:41:30 +0600

    commited

--- CountryProject/app/src/main/java/com/example/countryproject/Home.java
+++ CountryProject/app/src/main/java/com/example/countryproject/Home.java
@@ -2,13 +2,47 @@ package com.example.countryproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+    Button bd, india, pakistan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bd = (Button) findViewById(R.id.bdbtn);
+        Intent intent = new Intent(Home.this, information.class);
+        bd.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                intent.putExtra("name", "bd");
+                startActivity(intent);
+            }
+        });
+        india = (Button) findViewById(R.id.indiabtn);
+        india.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                intent.putExtra("name", "india");
+                startActivity(intent);
+            }
+        });
+        pakistan = (Button) findViewById(R.id.pakistanbtn);
+        pakistan.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                intent.putExtra("name", "pakistan");
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- CountryProject/app/src/main/java/com/example/countryproject/information.java
+++ CountryProject/app/src/main/java/com/example/countryproject/information.java
@@ -2,13 +2,48 @@ package com.example.countryproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class information extends AppCompatActivity {
+    ImageView btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        Bundle bndl = getIntent().getExtras();
+
+        txt = (TextView) findViewById(R.id.text);
+        btn = (ImageView) findViewById(R.id.img);
+        try {
+            String name = bndl.getString("name");
+            if(name.equals("bd"))
+            {
+                btn.setImageResource(R.drawable.bdgov);
+                txt.setText(R.string.bddetails);
+            }
+            else if(name.equals("india"))
+            {
+                btn.setImageResource(R.drawable.indiagov);
+                txt.setText(R.string.indiadetails);
+            }
+            else
+            {
+                btn.setImageResource(R.drawable.pakistangov);
+                txt.setText(R.string.pakistandetails);
+            }
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(this, "Reasource could not found", Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 }
